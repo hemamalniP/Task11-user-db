c37da4ede673799cef116713fe11da3da5c4b7a5
hemamalniP <87513027+hemamalniP@users.noreply.github.com>
2021-07-29 16:03:17 +0530

    Question 2
    Implement PUT method API for updating user information. And pass the user_id as param
    and other information which needs to be update as input.

---
+++ userupdate.py
@@ -0,0 +1,32 @@
+from flask import Flask,request,jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user1:user10@localhost:5432/empdb'
+app.debug=True
+db = SQLAlchemy(app)
+class usr(db.Model):
+    __tablename__='user'
+    user_id = db.Column('id', db.Integer, primary_key=True)
+    user_name = db.Column(db.String(100),nullable=False,unique=True)
+    password = db.Column(db.String(50),nullable=False)
+    company_name = db.Column(db.String(200),nullable=False)
+    phone_number = db.Column(db.Integer,nullable=False)
+    status = db.Column(db.Boolean,default=1)
+    def __init__(self,user_id,user_name,password,company_name,phone_number,status):
+        self.user_id=user_id
+        self.user_name=user_name
+        self.password=password
+        self.company_name=company_name
+        self.phone_number=phone_number
+        self.status=status
+@app.route('/up',methods=['PUT'])
+def upd():
+    value=request.get_json()
+    db.session.query(usr).filter(usr.user_id==value['val']).update({usr.phone_number:value['val2']})
+    db.session.commit()
+    return ("succesfully updated")
+
+if __name__ == '__main__':
+    app.run(debug=True)
